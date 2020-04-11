a335934c4ef35eff3fbcb6df5eefe302bdeda957
pullso <p.dubitskiy@ya.ru>
2020-04-11 20:58:01 +0500

    add functional add income and outcome

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Form @submitForm="onFormSubmit" />
     <TotalBalance :total="totalBalance" />
     <BudgetList :list="list" @deleteItem="onDeleteItem" />
   </div>
@@ -8,12 +9,14 @@
 <script>
 import BudgetList from '@/components/BudgetList';
 import TotalBalance from '@/components/TotalBalance';
+import Form from '@/components/Form';
 
 export default {
   name: 'App',
   components: {
     BudgetList,
-    TotalBalance
+    TotalBalance,
+    Form,
   },
   data() {
     return {
@@ -22,27 +25,34 @@ export default {
           type: 'INCOME',
           value: 10000,
           comment: 'Разработка сайта',
-          id: 1
+          id: 1,
         },
         2: {
           type: 'OUTCOME',
           value: -2500,
           comment: 'Услуги дизайнера ',
-          id: 2
-        }
-      }
+          id: 2,
+        },
+      },
     };
   },
   computed: {
     totalBalance() {
       return Object.values(this.list).reduce((acc, item) => acc + item.value, 0);
-    }
+    },
   },
   methods: {
     onDeleteItem(id) {
       this.$delete(this.list, id);
-    }
-  }
+    },
+    onFormSubmit(data) {
+      const newObj = {
+        ...data,
+        id: String(Math.random()),
+      };
+      this.$set(this.list, newObj.id, newObj);
+    },
+  },
 };
 </script>
 
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,13 +1,62 @@
 <template>
-  <ElCart>
-    <ElForm></ElForm>
-  </ElCart>
+  <ElCard class="form-card">
+    <ElForm :model="formData" ref="addItemForm" :rules="rules" label-position="right">
+      <ElFormItem label="Тип" prop="type">
+        <ElSelect class="type-select" v-model="formData.type" placeholder="Доход">
+          <ElOption label="Доход" value="INCOME"></ElOption>
+          <ElOption label="Расход" value="OUTCOME"></ElOption>
+        </ElSelect>
+      </ElFormItem>
+      <ElFormItem label="Комментарий" prop="comment">
+        <ElInput v-model="formData.comment"></ElInput>
+      </ElFormItem>
+      <ElFormItem label="Значение" prop="value">
+        <ElInput v-model.number="formData.value"></ElInput>
+      </ElFormItem>
+      <ElButton type="primary" @click="onSubmit">Добавить</ElButton>
+    </ElForm>
+  </ElCard>
 </template>
 
 <script>
 export default {
-  name: 'form'
+  name: 'Form',
+  data() {
+    return {
+      formData: {
+        type: 'INCOME',
+        comment: '',
+        value: 0,
+      },
+      rules: {
+        type: [{ required: true, message: 'Выберит тип операции', trigger: 'blur' }],
+        comment: [{ required: true, message: 'Пожалуйста, введие комментарий', trigger: 'change' }],
+        value: [
+          { required: true, message: 'Введите значене операции', trigger: 'change' },
+          { type: 'number', message: 'Значение должно быть числом' },
+        ],
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$refs.addItemForm.validate(valid => {
+        if (valid) {
+          this.$emit('submitForm', { ...this.formData });
+          this.$refs.addItemForm.resetFields();
+        }
+      });
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.form-card {
+  max-width: 500px;
+  margin: auto;
+}
+.type-select {
+  width: 100%;
+}
+</style>
